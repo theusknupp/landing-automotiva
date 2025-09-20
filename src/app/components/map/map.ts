936a89399cd4954a64d0f7e2b6ee48aa5739f937
theusknupp <matheus.knupp.ds@outlook.com>
2025-09-20 12:27:14 -0300

    Footer

--- src/app/components/map/map.ts
+++ src/app/components/map/map.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Footer } from '../footer/footer';
+import { Contact } from '../contact/contact';
 
 @Component({
   selector: 'app-map',
-  imports: [Footer],
+  imports: [Contact, Footer],
   templateUrl: './map.html',
   styleUrl: './map.css'
 })
