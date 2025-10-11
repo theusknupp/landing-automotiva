51161c4d5689a92753efe8144ebeb3e511196b01
theusknupp <matheus.knupp.ds@outlook.com>
2025-10-11 14:38:33 -0300

    ScrollTo Products + Scroll Smoth

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(
-      routes,
-      withInMemoryScrolling({
-        //Função para resolver o problema ao redirecionar para o produtos vindo da home e a página abrir no final do scroll
-                // 'enabled': Rola para o topo, mas memoriza a posição ao usar os botões de voltar/avançar do navegador.
-        scrollPositionRestoration: 'enabled', 
-      })
-    ),
-  ],
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes)
+  ] 
 };
--- src/app/components/products/products.ts
+++ src/app/components/products/products.ts
@@ -9,5 +9,9 @@ import { Footer } from '../footer/footer';
   styleUrl: './products.css'
 })
 export class Products {
-
+    ngOnInit(): void {
+    // Força a janela a rolar para o topo (coordenadas x:0, y:0)
+    // Para corrigir scroll ao vir da pagina works para products e o scroll estar no fim da página
+    window.scrollTo(0, 0); 
+  }
 }
